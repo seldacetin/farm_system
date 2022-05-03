501b59c212a08bd22452905f9dc7f1f0c56d289d
seldacetin <64745949+seldacetin@users.noreply.github.com>
2022-05-03 13:50:20 +0300

    farm_system

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>ÇIFTLIK SISTEMLERI</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="img/1.jpeg">
+</head>
+<body>
+
+</body>
+</html>
