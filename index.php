b55a651402b7c297bd81969979dac2e756248bbd
seldacetin <64745949+seldacetin@users.noreply.github.com>
2022-05-09 10:59:31 +0300

    farm_system

--- index.php
+++ index.php
@@ -1,14 +1,34 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="tr">
 <head>
     <title>ÇIFTLIK SISTEMLERI</title>
-    <link rel="stylesheet" href="/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/css/login.css">
-    <link rel="stylesheet" href="/img/2.jpeg">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="img/3.jpg">
 </head>
-<body class="resim">
-<div>
+<body class="arkaplan">
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-4 giris">
+            <form class="row">
+                <div class="input-group mb-3">
+                    <input type="email" placeholder="E-Mail">
+                </div>
+                <div class="input-group mb-3">
+                    <input type="email" placeholder="E-Mail">
+                </div>
+            </form>
 
+        </div>
+        <div class="col-md-4 "></div>
+        <div class="col-md-4"></div>
+    </div>
 </div>
+
+<script type="text/javascript" src="js/jquery-3.6.0.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+
 </body>
 </html>
